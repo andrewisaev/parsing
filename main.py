6aa09a6b860a8a60691d9ad835de28003eb5a0bf
andrey.isaev <an.isaev7@gmail.com>
2022-05-18 15:52:00 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import requests
 from bs4 import BeautifulSoup
 
-with open("blank/index.html") as file:
+url = 'https://yandex.ru/news/rubric/politics'
+response= requests.get(url)
+src=response.text
+
+with open('index.html', 'w') as file:
+    file.write(src)
+
+with open('index.html') as file:
     src = file.read()
 
-soup = BeautifulSoup(src, "lxml")
-title = soup.title
-print(title)
+soup = BeautifulSoup(src, 'lxml')
+
+all_a = soup.find_all(class_='mg-card__link')
+for item in all_a:
+    item_title = item.text
+    item_href = item.get("href")
+    print(f'{item_title}: {item_href}')
